e9f9fe5e84e00b4b9a3ea8a0ffd870066f427555
Kunals <kunalshinde0612@gmail.com>
2024-05-02 05:35:41 +0530

    commit

--- assignment9.asm
+++ assignment9.asm
@@ -1,135 +1,356 @@
-Assignment no 9 :- Factorial
-; Microprocessor Lab
-; Assignment no. : 9
-;Problem Statement: Write x86 ALP to find the factorial of a given integer number on a command line by using
-;recursion. Explicit stack manipulation is expected in the code.
+;perform overlapped  block transfer with string instruction
 
-%macro cmn 4			;common macro for input/output
-	mov rax,%1
-	mov rdi,%2
-	mov rsi,%3
-	mov rdx,%4
-	syscall
+
+ section .data
+
+ 	array db 10h,20h,30h,40h,50h
+ 	msg1: db 'Before overlapped :',0xa
+	len1: equ $-msg1
+
+ 	msg2: db 'After overlapped(using string instruction) :',0xa
+ 	len2: equ $-msg2
+
+        msg3: db ' ',0xa
+ 	len3: equ $-msg3
+     
+        msg4: db ' : '
+	len4: equ $-msg4
+       
+     
+        count db 0
+        count1 db 0
+        count2 db 0
+        count3 db 0
+        count4  db 0
+        count5  db 0
+ section .bss
+  	addr resb 16
+        num1 resb 2
+       
+
+%macro print 2      pranavdeshpande108@gmail.com
+mov rax,01
+mov rdi,01
+mov rsi,%1
+mov rdx,%2
+syscall
 %endmacro
 
-section .data
-	num db 00h
-	msg db "Factorial is : "
-	msglen equ $-msg
-	msg1 db "*****Program to find Factorial of a number***** ",0Ah
-		db "Enter the number : ",
-	msg1len equ $-msg1
-	msgth db 0Ah,"Thank you for using program ",0Ah
-	msgthlen equ $-msgth
-	zerofact db " 00000001 "
-	zerofactlen equ $-zerofact
 
-section .bss
-	dispnum resb 16
-	result resb 4
-	temp resb 3
+ section .text
+ global _start
+
+
+ _start:
+ 	;*************** print address and acutal number from array ******************
+ 	;print msg    
+ 	print msg1,len1
+
+ 	xor rsi,rsi		;clear rsi pointer
+
+ 	mov rsi,array		;point rsi to array
+ 	mov byte[count],05	;set counter as 5
+
+     up:mov rbx,rsi	;move contern of rsi to rbx
+ 	push rsi		;stored rsi address to stack
+        mov rdi,addr	;point rdi to addr buffer
+ 	call HtoA1		;print address(16 digit)
+ 	pop rsi
+
+
+ 	mov dl,[rsi]		;print number (2 digit)
+ 	push rsi
+        mov rdi,num1
+	call HtoA2
+ 	pop rsi
+
+	 inc rsi
+
+	 dec byte[count]
+	 jnz up
+
+ ;****************moving array element to to after 10 position***************    
+        mov rsi,array
+        mov rdi,array+03h
+	mov byte[count3],05h
+
+ loop10:mov dl,00h
+ 	movsb
+        dec byte[count3]
+        jnz loop10
+  ;**************** after 3 position(actual overlapping)*************** 
+        xor rsi,rsi
+
+        mov rsi,arrayh	  ;overlapping on position 3
+    ;    mov rdi,array+03h	;to print all content [3(before)+5(after)]=10
+ 	mov byte[count5],05h    ;moving 5 digit
+
+ loop11:mov dl,byte[rdi]
+        mov byte[rsi],dl
+        inc rsi
+        inc rdi
+        dec byte[count5]
+        jnz loop11
+	
+        print msg1,len2
 	
 
-section .text
-global _start
-_start:
-	
-	cmn 1,1,msg1,msg1len
-	cmn 0,0,temp,3			;accept number from user
-	call convert			;convert number from ascii to hex
-	mov [num],dl
-	
-	cmn 1,1,msg,msglen
-	
-	xor rdx,rdx
-	xor rax,rax
-	mov al,[num]			;store number in accumulator
-	cmp al,01h			
-	jbe endfact
-	xor rbx,rbx
-	mov bl,01h
-	call factr			;call factorial procedure
-	call display
+        xor rsi,rsi
+        mov rsi,array
+        mov byte[count4],08h
 
-	call exit
-endfact:
-	cmn 1,1,zerofact,zerofactlen
-	call exit
+   up10:mov rbx,rsi
+ 	push rsi
+        mov rdi,addr
+ 	call HtoA1
+ 	pop rsi
 
-	factr:				;recursive procedure
-	
-			cmp rax,01h
-			je retcon1
-			push rax			
-			dec rax
-			
-			call factr
+	mov dl,[rsi]
+ 	push rsi
+        mov rdi,num1
+ 	call HtoA2
+ 	pop rsi
 
-		retcon:
-		
-			pop rbx
-		mul ebx	
-			jmp endpr
+	inc rsi
 
-		retcon1:			;if rax=1 return
-			pop rbx
-			jmp retcon		
-		endpr:
-	
+	dec byte[count4]
+ 	jnz up10
+               
+
+        mov rax,60
+	mov rdi,0
+	syscall 
+
+
+;*************** print procedure for address **************
+   HtoA1:mov byte[count1],16
+    dup1:rol rbx,4
+        mov al,bl
+        and al,0fh
+    	cmp al,09
+ 	jg p3
+ 	add al,30h
+ 	jmp p4
+    p3: add al,37h
+    p4: mov [rdi],al
+        inc rdi
+        dec byte[count1]
+        jnz dup1
+
+ 
+	 print addr,16
+	 print msg4,len4
+
+ 	 ret
+
+;*************** print procedure for data **************
+   HtoA2: mov byte[count2],02
+   dup2: rol dl,04
+	 mov al,dl
+	 and al,0fh
+	 cmp al,09h
+	 jg p31
+	 add al,30h
+	 jmp p41
+
+   p31:  add al,37h
+   p41:  mov [rdi],al
+   
+        inc rdi
+        dec byte[count2]
+        jnz dup2
+        
+        print num1,02
+        print msg3,len3
+
+        ret
+
+
+
+
+
+;******output*******
+;nasm -f elf64 over.asm
+;ld -o a over.o
+; ./a
+;Before overlapped :
+;000000000060029C : 10
+;000000000060029D : 20
+;000000000060029E : 30
+;000000000060029F : 40
+;00000000006002A0 : 50
+;After overlapped (using string instruction) :
+;000000000060029C : 10
+;000000000060029D : 20
+;000000000060029E : 30
+;000000000060029F : 10
+;00000000006002A0 : 20
+;00000000006002A1 : 30
+;00000000006002A2 : 40
+;00000000006002A3 : 50
+
+
+
+
+
+
+
+
+
+;perform nonoverlapped  block transfer with string instruction
+ section .data
+	 array db 10h,20h,30h,40h,50h
+        msg1: db 'Before overlapped :',0xa
+ 	len1: equ $-msg1
+
+	msg2: db 'After overlapped :',0xa
+ 	len2: equ $-msg2
+
+        msg3: db ' ',0xa
+	len3: equ $-msg3
+     
+        msg4: db ' : '
+	len4: equ $-msg4
+    
+        count db 0
+        count1 db 0
+        count2 db 0
+        count3 db 0
+        count4  db 0
+ 
+ section .bss
+	 addr resb 16
+         num1 resb 2
+       
+	%macro print 2
+	mov rax,01
+	mov rdi,01
+	mov rsi,%1
+	mov rdx,%2
+	syscall
+	%endmacro
+ section .text
+ global _start
+ _start:print msg1,len2             ;print msg
+        xor rsi,rsi                 ;clear rsi
+
+        mov rsi,array               ;point rsi to array
+        mov byte[count],05          ;counter
+
+    up: mov rbx,rsi                  ;copy address -> rbx
+        push rsi                      
+        mov rdi,addr                ;point rdi to addr variable
+        call HtoA1                  ;call h-A proc to print 64bit address
+        pop rsi
+
+        mov dl,[rsi]                ;copy actual number-> dl 
+        push rsi
+        mov rdi,num1                ;point rdi to num variable
+        call HtoA2                  ;copy H-A proc to print 8 bit number
+        pop rsi
+
+        inc rsi
+
+        dec byte[count]
+        jnz up
+
+     
+        mov rsi,array
+        mov rdi,array+5h
+        mov byte[count3],05h
+
+       loop10:  movsb                     ;copy content of address pointed by rsi to memory location pointed by rdi
+        dec byte[count3]
+        jnz loop10
+
+        print msg2,len2
+        
+        mov rsi,array
+        mov byte[count4],0Ah
+
+   
+
+   up10:mov rbx,rsi
+        push rsi
+        mov rdi,addr
+        call HtoA1
+        pop rsi
+
+        mov dl,[rsi]
+        push rsi
+        mov rdi,num1
+        call HtoA2
+        pop rsi
+
+        inc rsi
+
+        dec byte[count4]
+        jnz up10
+               
+
+        mov rax,60
+        mov rdi,0
+        syscall 
+;*********************** HtoA1 ************************
+   HtoA1:mov byte[count1],16
+   dup1:rol rbx,4
+ 	mov al,bl
+	 and al,0fh
+	 cmp al,09
+	 jg p3
+	 add al,30h
+	 jmp p4
+     p3: add al,37h
+      p4:mov [rdi],al
+	 inc rdi
+        dec byte[count1]
+        jnz dup1
+    
+        print addr,16
+        print msg4,len4
+
 	ret
+**************************HtoA2*************
+HtoA2:   mov byte[count2],02
+dup2:    rol dl,04
+	 mov al,dl
+	 and al,0fh
+	 cmp al,09h
+	 jg p31
+	 add al,30h
+ 	 jmp p41
 
-	display:			; procedure to convert hex to ascii
-	
-			mov rsi,dispnum+15
-			xor rcx,rcx
-			mov cl,16
+    p31: add al,37h
+     p41:mov [rdi],al
 
-		cont:
-			xor rdx,rdx
-			xor rbx,rbx
-			mov bl,10h
-			div ebx
-			cmp dl,09h
-			jbe skip
-			add dl,07h
-		skip:
-			add dl,30h
-			mov [rsi],dl
-			dec rsi
-			loop cont
-	
-			cmn 1,1,dispnum,16
-	
-	ret
+         inc rdi
+        dec byte[count2]
+        jnz dup2
+     
+        print num1,02
+        print msg3,len3
+         
+        ret
 
-	convert:			;procedure to convert ascii to hex
-			mov rsi,temp
-			mov cl,02h
-			xor rax,rax
-			xor rdx,rdx
-		contc:
-			rol dl,04h
-			mov al,[rsi]
-			cmp al,39h
-			jbe skipc
-			sub al,07h
-		skipc:
-			sub al,30h
-			add dl,al
-			inc rsi
-			dec cl
-			jnz contc
-	
-	ret
-
-	exit:				;exit system call
-			cmn 1,1,msgth,msgthlen	
-			mov rax,60
-			mov rdi,0
-			syscall
-
-	ret
-
-
-
-
+;*****************************************************
+;************output*********
+;nasm -f elf64 w_over.asm
+;ld -o w_over w_over.o
+; ./w_over
+;Before overlapped :
+;0000000000600264 : 10
+;0000000000600265 : 20
+;0000000000600266 : 30
+;0000000000600267 : 40
+;0000000000600268 : 50
+;After overlapped :
+;0000000000600264 : 10
+;0000000000600265 : 20
+;0000000000600266 : 30
+;0000000000600267 : 40
+;0000000000600268 : 50
+;0000000000600269 : 10
+;000000000060026A : 20
+;000000000060026B : 30
+;000000000060026C : 40
+;000000000060026D : 50
